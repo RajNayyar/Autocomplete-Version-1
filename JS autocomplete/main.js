2a3c02ccd32352ede0b93ce87b83c4e477efa2d3
Rajpreet Singh Nayyar <rnayyar@tavisca.com>
2018-08-22 22:56:17 +0530

    implemented javascript to create an suggestion/autocomplete form

---
+++ JS autocomplete/main.js
@@ -0,0 +1,39 @@
+function suggestion(name){     
+       var names = [
+       "Ragul", "Rajpreet", "Pallvi", "Neha", "Ankita", "Raja", "Shreea", "Smriti", "Shrijeet", "Ayush", "Swapnil", "Nihit", "Bhargavi", "Anushka", "Swinal", "Utkarsh", "Saurabh", "Paarth", "Vishwas", "Mohit", "Gurbaksh", "Ashwarya"];
+           names.sort();
+           if(name.length==0) {
+               var myNode = document.getElementById("list");
+               myNode.innerHTML = '';
+           }
+         else {
+           var myNode = document.getElementById("list");
+                   myNode.innerHTML = '';
+
+               var ul = document.getElementById("list");
+
+           for (var i = 0; i < names.length; i++) {
+                  var lowercasename = names[i].toLowerCase();
+               if(lowercasename.indexOf(name.toLocaleLowerCase()) != -1){
+               var li = document.createElement("li");
+               li.setAttribute("onclick", "display('"+names[i]+"')");
+               ul.appendChild(li);
+               li.innerHTML = names[i];
+                   flag=1;
+               }
+           }
+           if(flag==0) {
+                var li = document.createElement("li");
+                ul.appendChild(li);
+                li.innerHTML = "Nothing Found";
+                }
+            }
+        }
+       function display(name) {
+           document.getElementById("type-here").value = name;
+           document.getElementById("list").innerHTML="";
+       }
+       function onClickBlur() {
+          document.getElementById("list").innerHTML="";
+       }
+
